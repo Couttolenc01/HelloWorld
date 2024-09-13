1c9b1ebe48d8f44e00849dc749b482cf62b5cd08
Couttolenc01 <A01754959@tec.mx>
2024-09-13 10:39:13 -0600

    tercer commit

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Importa el módulo HTTP
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Crea el servidor
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-// Define el puerto y arranca el servidor
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  
 });
